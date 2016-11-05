d7715cd0b830eba8ae346dc74d8bd445fe7a0556
Michael Le-Reiver <m.reiver@gmail.com>
2016-11-05 11:35:20 -0700

    Updated navigation and fixed header.

--- index.php
+++ index.php
@@ -17,27 +17,28 @@
     <link rel="stylesheet" href="includes/styles/reset.css.php">
     <link rel="stylesheet" href="includes/styles/helper.css.php">
     <link rel="stylesheet" href="includes/styles/style.css">
+    <script src="includes/scripts/jquery-1.11.0.min.js"></script>
+
 </head>
 
 <body>
     <header>
         <nav>
-            <ul>
-                <li><a href="#">logo</a></li> 
-                <a href="index.php"><h1>Burrito Masala</h1></a>
-            </ul>
+<!--            <a href="#" id="logo">logo</a>-->
+            <a href="#"><h1>Burrito Masala</h1></a>
         </nav>
         <h2>A fresh &amp; exotic take on <br> Portland Street Food</h2>
     </header>
     
     <main>
-        <location>
+        <section id="location">
             <h1>Find Us Here</h1>
-            <map><img align="center" src="includes/images/map.png">
+            <map>
+                <img align="center" src="includes/images/map.png">
             </map>
             <p>313 SW Alder, Portland, OR<br>We are open 7 days a week<br>11am-7pm</p>
-        </location>
-        <gallery>
+        </section>
+        <section id="gallery">
             <h1>Our Food</h1>
             <ul>
                 <li><img src="includes/images/meatBurrito.jpg">Meat Burrito</li>
@@ -49,19 +50,36 @@
                 <li><img src="includes/images/indianBowl.jpg">Indian Bowl</li>
                 <li><img src="includes/images/fifthStreet.jpg">3rd Street Carts</li>
             </ul>
-        </gallery>
-        <ordering>
+        </section>
+        <section id="ordering">
             <h1>How To Order</h1>
+            <h3>1. Choose either Burrito or Bowl</h3>
+            <h3>2. Choose either Meat or Veggie</h3>
+            <h3>3. Choose type of Rice</h3>
+            <h3>4. Choose your Toppings</h3>
             <h2>Meat</h2>
             <h2>Veggie</h2>
             <h2>Rice</h2>
             <h2>Toppings</h2>
-        </ordering>
+        </section>
     </main>
     
     <footer>
     </footer>
-    
-    
+
+<script>
+    $('a[href^="#"]').on('click', function(event) {
+
+        var target = $(this.getAttribute('href'));
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+
+    });
+</script>
 </body>
 </html>
