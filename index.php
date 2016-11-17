946eba18c1979726a13b80aa7b8d40c271adb5a0
Michael Le-Reiver <m.reiver@gmail.com>
2016-11-17 12:51:29 -0800

    Desktop column cleanup

--- index.php
+++ index.php
@@ -23,84 +23,91 @@
     <!--Header section-->
     <header>
         <nav>
-            <a href="#top"><h1>Burrito Masala</h1></a>
+            <a href="#top"><h1><span id="burrito">Burrito</span><span id="masala">Masala</span></h1></a>
         </nav>
         <h2>A Fresh &amp; Exotic Take on <br> Portland Street Food</h2>
     </header>
     <main>
-            <!--Description section-->
-<!--        <section id="description">-->
-<!--            <h1>Southern Indian and Mexican foods... stuffed in a flour tortilla or in a bowl!</h1>-->
-<!--        </section>-->
-        <!--Ordering section-->
-        <section id="ordering" >
+        <section id="ordering">
             <h1>How To Order</h1>
-            <h3>1. Choose either Burrito or Bowl <span id="price">$8</span></h3>
-            <h3>2. Choose either Meat or Veggie</h3>
-            <h3>3. Choose type of Rice</h3>
-            <h3>4. Choose your Toppings</h3>
-            <h2>Meat</h2>
-            <ul>
-                <li>Chicken Tikka Masala</li>
-                <li>Lamb Curry</li>
-                <li>Pork in Salsa Verde</li>
-            </ul>
-            <h2>Veggie</h2>
-            <ul>
-                <li>Sambar (lentil curry, vegan)</li>
-                <li>Aloo Gobi Masala <br>&nbsp;&nbsp;(potato and cauliflower, vegan)</li>
-                <li>Chile Relleno <br>&nbsp;&nbsp;(vegetarian, contains cheese)</li>
-            </ul>
-            <h2>Rice (all vegan)</h2>
-            <ul>
-                <li>Kerala Biryani</li>
-                <li>Spanish Rice</li>
-                <li>White or Brown Rice (khaima)</li>
-            </ul>
-            <h2>Toppings</h2>
-            <ul class="left">
-                <li>Mango Chutney</li>
-                <li>Tomatillo Salsa</li>
-                <li>Raita</li>
-                <li>Sour cream</li>
-                <li>Guacamole</li>
-                <li>&nbsp;&nbsp;(<span id="price">$.75</span> extra!)</li>
-            </ul>
-            <ul class="right">
-                <li>Cheese</li>
-                <li>Sour cream</li>
-                <li>Onions</li>
-                <li>Tomatoes</li>
-                <li>Cucumbers</li>
-                <li>Cilantro</li>
-            </ul>
+            <h3><span id="steps">1.</span> Choose either Burrito or Bowl <span id="price">$8</span></h3>
+            <h3><span id="steps">2.</span> Choose choice of Meat or Veggie</h3>
+            <h3><span id="steps">3.</span> Choose type of Rice</h3>
+            <h3><span id="steps">4.</span> Choose your Toppings</h3>
+            <div class="section group">
+                <div  class="col span_6_of_12">
+                    <h2>Meats</h2>
+                    <ul>
+                        <li>Chicken Tikka Masala</li>
+                        <li>Lamb Curry</li>
+                        <li>Pork in Salsa Verde</li>
+                    </ul>
+                </div>
+            <div  class="col span_6_of_12">
+                <h2>Veggies</h2>
+                <ul>
+                    <li>Sambar (lentil curry, vegan)</li>
+                    <li>Aloo Gobi Masala <br>&nbsp;&nbsp;(potato and cauliflower, vegan)</li>
+                    <li>Chile Relleno <br>&nbsp;&nbsp;(vegetarian, contains cheese)</li>
+                </ul>
+            </div>
+            <div  class="col span_6_of_12">
+                <h2>Rice (all vegan)</h2>
+                <ul>
+                    <li>Kerala Biryani</li>
+                    <li>Spanish Rice</li>
+                    <li>White or Brown Rice (khaima)</li>
+                </ul>
+            </div>
+            <div  class="col span_6_of_12">
+                <h2>Toppings</h2>
+                <ul class="left">
+                    <li>Mango Chutney</li>
+                    <li>Tomatillo Salsa</li>
+                    <li>Raita</li>
+                    <li>Sour cream</li>
+                    <li>Guacamole</li>
+                    <li>&nbsp;&nbsp;(<span id="price">$.75</span> extra!)</li>
+                </ul>
+                <ul class="right">
+                    <li>Cheese</li>
+                    <li>Sour cream</li>
+                    <li>Onions</li>
+                    <li>Tomatoes</li>
+                    <li>Cucumbers</li>
+                    <li>Cilantro</li>
+                </ul>
             <ul class="bottom">
                 <li>Grandma Nena’s Fiery Red Salsa </li>
             </ul>
         </section>
+        <!--Gallery section -->
         <section id="gallery">
             <div class="section group">
-                <div id="column" class="col span_4_of_12"><img src="includes/images/vegBurrito.jpg" alt="Sambar Burrito"></div>
-                <div id="column" class="col span_4_of_12"><img src="includes/images/riceBowl.jpg" alt="Lamb Curry Bowl"></div>
-                <div id="column" class="col span_4_of_12"><img src="includes/images/fifthStreet.jpg" alt="3rd Street Carts"></div>
+                <div id="column" class="col span_6_of_12"><img src="includes/images/vegBurrito.jpg" alt="Sambar Burrito"></div>
+                <div id="column" class="col span_6_of_12"><img src="includes/images/riceBowl.jpg" alt="Lamb Curry Bowl"></div>
             </div>
         </section>
         <!--Location section-->
         <section id="location" class="section group">
             <h1>Find Us Here</h1>
-            <map>
-                <a href="https://www.google.com/maps/place/313+SW+Alder+St,+Portland,+OR+97204/@45.5190208,-122.675541,17z/data=!3m1!4b1!4m5!3m4!1s0x54950a05c1ee2db9:0xce7ecf88b6d8e74b!8m2!3d45.5190208!4d-122.675541" target="_blank" >
-                    <img  class="col_map span_6_of_12"  src="includes/images/map.png"></a>
-            </map>
-            <p >313 SW Alder, Portland, OR<br>We are open 7 days a week<br>11am-7pm</p>
+            <div class="col span_6_of_12">
+                <map>
+                    <a href="https://www.google.com/maps/place/313+SW+Alder+St,+Portland,+OR+97204/@45.5190208,-122.675541,17z/data=!3m1!4b1!4m5!3m4!1s0x54950a05c1ee2db9:0xce7ecf88b6d8e74b!8m2!3d45.5190208!4d-122.675541" target="_blank" >
+                        <img src="includes/images/map.png"></a>
+                </map>
+                <p >313 SW Alder, Portland, OR<br>We are open 7 days a week<br>11am-7pm</p>
+                </div>
+            <div  class="col span_6_of_12">
+                <img src="includes/images/fifthStreet.jpg" alt="3rd Street Carts">
+            </div>
         </section>
     </main>
     <!-- Footer section-->
-    <footer  class="section group">
+    <footer  class="section group" >
         <section id="bois">
-            <div  class="col span_6_of_12">
                 <h3>Our Story</h3>
-            <div  class="col span_12_of_12">
+            <div  class="col span_6_of_12">
                 <h4>Prasad</h4>
                 <p>Champollion cosmos preserve and cherish that pale blue dot tesseract, another world?
                     Great turbulent clouds the ash of stellar alchemy cosmic ocean? Finite but unbounded
@@ -108,50 +115,44 @@
                     upon billions upon billions.
                 </p>
             </div>
-            <div  class="col span_12_of_12">
+            <div  class="col span_6_of_12">
                 <h4>Ramona</h4>
                 <p>Hypatia realm of the galaxies a very small stage in a vast
                     cosmic arena! Birth! Cosmos a mote of dust suspended in a sunbeam, cosmos. Light
                     years rich in heavy atoms, paroxysm of global death rich in mystery courage of our
-                    questions decipherment, the only home we've ever known shores of the cosmic ocean!
+                    questions decipherment.
                 </p>
             </div>
         </section>
-        <section id="social" class="col span_6_of_12">
-            <div  >
-                <h3>Be Social</h3>
-            </div>
-            <div  class="col span_3_of_12">
-                <a href="https://twitter.com/?lang=en" target="_blank"><img src="includes/images/twitter_wht.png"></a>
-            </div>
-            <div  class="col span_3_of_12">
-                <a href="https://www.facebook.com/" target="_blank"><img src="includes/images/facebook_wht.png"></a>
-            </div>
-            <div  class="col span_3_of_12">
-                <a href="https://yelp.com/?lang=en" target="_blank"><img src="includes/images/yelp_wht.png"></a>
-            </div>
-            <div  class="col span_3_of_12">
-                <a href="https://www.pinterest.com/" target="_blank"><img src="includes/images/pinterest_wht.png"></a>
-            </div>
-        </section>
-        <section id="phone"  class="section group">
-            <div  class="col span_6_of_12">
-                <h3>Text In Your Order</h3>
-                <a href="sms:503-555-5555"><img src="includes/images/text_wht.png"></a>
-            </div>
-            <div  class="col span_6_of_12">
-                <h3>Call In Your Order</h3>
-                <a href="tel:503-555-5555"><img src="includes/images/talk_wht.png"></a>
-            </div>
-        </section>
-        <section id="phone_desktop">
-            <div>
-                <h3>Call Us</h3>
-                <p>503-555-5555</p>
-            </div>
-        </section>
+        <div  class="section group">
+            <section id="phone" class="col span_6_of_12">
+                <div id="text">
+                    <h3>Text In Your Order</h3>
+                    <a href="sms:503-555-5555"><img src="includes/images/text_wht.png"></a>
+                </div>
+                <div id="talk">
+                    <h3>Call In Your Order</h3>
+                    <a href="tel:503-555-5555"><img src="includes/images/talk_wht.png"></a>
+                </div>
+            </section>
+            <section id="phone_desktop" class="col span_6_of_12">
+                <div>
+                    <h3>Call Us</h3>
+                    <p>503-555-5555</p>
+                </div>
+            </section>
+            <section id="social" class="col span_6_of_12">
+                <div>
+                    <h3>Be Social</h3>
+                </div>
+                    <a href="https://twitter.com/?lang=en" target="_blank"><img src="includes/images/twitter_wht.png"></a>
+                    <a href="https://www.facebook.com/" target="_blank"><img src="includes/images/facebook_wht.png"></a>
+                    <a href="https://yelp.com/?lang=en" target="_blank"><img src="includes/images/yelp_wht.png"></a>
+                    <a href="https://www.pinterest.com/" target="_blank"><img src="includes/images/pinterest_wht.png"></a>
+            </section>
+        </div>
         <section id="copyright">
-            <p>&copy; 2016 Burrito Masala</p>
+            <p>&copy; 2016 Burrito Masala <br>Designed by Le-Reiver</p>
         </section>
     </footer>
 <?php require('includes/smooth_scroll.php');?>
